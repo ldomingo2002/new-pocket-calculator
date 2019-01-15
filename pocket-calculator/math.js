dd758352441e7d2cf80f339462c05d444ff87006
Leslie Dominguez <LeslieDominguez601@Gmail.com>
2019-01-15 00:14:47 -0500

    added old code and added a few things. Just need to figure out how to work the functions and focus on the css later

---
+++ pocket-calculator/math.js
@@ -0,0 +1,83 @@
+
+function ac(){
+
+let p = document.getElementById('display').innerHTML = "0";
+
+}
+
+function negation(){
+}
+
+function percentage(){
+}
+
+function division(){
+  let p = document.getElementById('display').innerHTML = "/"
+}
+
+function seven(){
+  let p = document.getElementById('display').innerHTML = "7";
+
+}
+
+function eight(){
+    let p = document.getElementById('display').innerHTML = "8";
+}
+
+function nine(){
+    let p = document.getElementById('display').innerHTML = "9";
+}
+
+function multiplication(){
+  let p = document.getElementById('display').innerHTML = "x"
+}
+
+function four(){
+  let p = document.getElementById('display').innerHTML = "4";
+}
+
+function five(){
+    let p = document.getElementById('display').innerHTML = "5";
+}
+
+function six(){
+    let p = document.getElementById('display').innerHTML = "6";
+}
+
+function subtraction(){
+  let p = document.getElementById('display').innerHTML = "-"
+}
+
+function one(){
+    let p = document.getElementById('display').innerHTML = "1";
+}
+
+function two(){
+    let p = document.getElementById('display').innerHTML = "2";
+}
+
+function three(){
+    let p = document.getElementById('display').innerHTML = "3";
+}
+
+function addition(){
+  let p = document.getElementById('display').innerHTML = "+"
+}
+
+function zero(){
+    let p = document.getElementById('display').innerHTML = "0";
+}
+
+function decimal(){
+  let p = document.getElementById('display').innerHTML = "."
+}
+
+function equals(){
+  let p = document.getElementById('display').innerHTML = "="
+}
+
+
+
+function mathMultiply (n1, n2){
+  return n1 * n2
+}
